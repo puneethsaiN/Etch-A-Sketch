d0df9248fd54a1a17c021f5bbefd7bd19b7ab4f2
puneethsaiN <101854505+puneethsaiN@users.noreply.github.com>
2024-09-17 14:06:59 +0530

    added change size button and added drag to color functin instead of drag

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const container = document.querySelector(".container");
-
 const eraseBtn = document.querySelector(".btn-erase");
+const sizeBtn = document.querySelector(".btn-size");
+let isActive = false;
+let containerSize = 600;
+
+sizeBtn.addEventListener("click", (e) => {
+    let numSquares;
+    while(!numSquares){
+        const input = parseInt(prompt("Enter number of squares in each side"));
+        if(input > 64){
+            alert("Size is too Large! Maximum allowed size is 64 squares");
+        }
+        else{
+            numSquares = input;
+        }
+    }
+    let allSquares = document.querySelectorAll(".square");
+    allSquares.forEach((box) => {
+        container.removeChild(box);
+    });
+    drawGrid(600, numSquares);
+});
 
 eraseBtn.addEventListener("click", (e) => {
     let allSquares = document.querySelectorAll(".square");
@@ -9,18 +29,40 @@ eraseBtn.addEventListener("click", (e) => {
     });
 });
 
-function drawGrid(n_row){
+container.addEventListener('mousedown', (e) => {
+    isActive = true;
+    e.preventDefault();
+});
+container.addEventListener('mouseup', () => {
+    isActive = false;
+});
+container.addEventListener('mouseleave', () => {
+    isActive = false;
+});
+
+function drawGrid(containerSize, n_row){
     for(let i=0;i<n_row*n_row;i++){
         const square = document.createElement("div");
         square.classList.add("square");
+
+        let squareSize = (containerSize - (n_row - 1)) / n_row;
+        square.style.height = `${squareSize}px`;
+        square.style.width = `${squareSize}px`;
     
-          square.addEventListener('mouseover', () => {
-              square.classList.add("marked"); // Dragging to color
-          });
-          container.appendChild(square);
+        square.addEventListener('mousemove', () => {
+            if(isActive){
+                square.classList.add("marked"); // Dragging to color
+            }
+        });
+        square.addEventListener('click', () => {
+            square.classList.add("marked"); // Dragging to color
+        });
+
+        container.appendChild(square);
     }
 }
 
-drawGrid(16);
+
+drawGrid(containerSize, 16);
 
 
